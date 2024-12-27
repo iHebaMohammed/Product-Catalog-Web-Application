14b748953d46df2115f3d505b9967a73084993e9
Heba Adel Ahmed <HebaAdelAhmedMohammed@gmail.com>
2024-12-27 02:19:00 +0200

    initial commit

---
+++ Demo.DAL/Contexts/ProductCategoryDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Demo.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Contexts
+{
+    public class ProductCategoryDbContext : DbContext
+    {
+        public ProductCategoryDbContext(DbContextOptions<ProductCategoryDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Electronics" },
+            new Category { Id = 2, Name = "Fashion" },
+            new Category { Id = 3, Name = "Accessorize" }
+            );
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Demo.DAL/Entities/Products.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public int DurationInDays { get; set; }
+        public decimal Price { get; set; }
+        public string ImagePath { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } // Navigation Property
+    }
+}
